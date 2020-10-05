6488f1deb08073d0597852a5fbec9c1f7bdff5a8
jtestares <jtestares@up.edu.ph>
2020-10-05 17:03:26 +0800

    edited port inputs for and, or

--- model/alu.vhd
+++ model/alu.vhd
@@ -15,8 +15,8 @@ architecture struc of alu is
     k0_ADD, k1_ADD, c_out_ADD, 
     k0_SUB_1, k1_SUB_1, c_out_SUB_1 : std_logic;
 begin
-    AND_OP: entity work.AND_OP(arch) port map (i0, j0, i1, j1, k0_AND, k1_AND, c_out_AND);
-    OR_OP: entity work.OR_OP(arch) port map (i0, j0, i1, j1, k0_OR, k1_OR, c_out_OR);
+    AND_OP: entity work.AND_OP(arch) port map (i0, j0, i1, j1, s0, k0_AND, k1_AND, c_out_AND);
+    OR_OP: entity work.OR_OP(arch) port map (i0, j0, i1, j1, s0, k0_OR, k1_OR, c_out_OR);
     ADD: entity work.ADD(arch) port map (i0, j0, i1, j1, k0_ADD, k1_ADD, c_out_ADD);
     SUB_1: entity work.SUB_1(arch) port map (i0, j0, i1, j1, k0_SUB_1, k1_SUB_1, c_out_SUB_1);
     MUX_k0: entity work.MUX(arch) port map(s0, s1, s2, k0_AND, k0_OR, '0', '0', k0_ADD, k0_SUB_1, '0', '0', k0);
