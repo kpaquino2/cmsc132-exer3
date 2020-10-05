5bf9b03e4876d04915c0e0f99165742985fa767d
Kyle Aquino <kpaquino2@up.edu.ph>
2020-10-05 13:25:21 +0800

    added model folder with alu.vhd

---
+++ model/alu.vhd
@@ -0,0 +1,18 @@
+library library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity alu is
+    port (
+        s0, s1, s2, i0, i1, j0, j1 : in std_logic;
+        k0, k1, c_out : out std_logic
+    );
+end entity alu;
+
+architecture struc of alu is
+    
+begin
+    
+    
+    
+end architecture struc;
